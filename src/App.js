5d748b200f83ba7800bd2a99ad64fbcc5104ad45
wathekk <wathek9711@gmail.com>
2020-08-18 14:35:07 +0100

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import { CardList } from './components/card-list/card-list.component.jsx';
-import { SearchBox } from './components/search-box/search-box.component.jsx';
+import { CardList } from "./components/card-list/card-list.component.jsx";
+import { SearchBox } from "./components/search-box/search-box.component.jsx";
 
-
-import './App.css';
+import "./App.css";
 
 class App extends React.Component {
-  constructor () {
+  constructor() {
     super();
-    
+
     this.state = {
       kawarjeya: [],
-      searchField: ""
-    }
+      searchField: "",
+    };
   }
 
-  componentDidMount () {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(users => this.setState({kawarjeya : users}))
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ kawarjeya: users }));
   }
 
-  render () {
+  render() {
     const { kawarjeya, searchField } = this.state;
-    const filteredKawarjeya = kawarjeya.filter(kawarji => 
-        kawarji.name.toLowerCase().includes(searchField.toLowerCase())
-    )
+    const filteredKawarjeya = kawarjeya.filter((kawarji) =>
+      kawarji.name.toLowerCase().includes(searchField.toLowerCase())
+    );
 
     return (
       <div className="App">
-     
-        <SearchBox 
+        <h1>Greatest Football Players</h1>
+
+        <SearchBox
           placeholder="search kawarji"
-          handleChange={ e => this.setState({searchField : e.target.value})}
-          />
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
 
         <CardList kawarjeya={filteredKawarjeya} />
       </div>
     );
   }
-
-
 }
 
 export default App;
