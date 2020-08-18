fc1b2257997d376ce303f6d1a19c40fdc31ec908
wathekk <wathek9711@gmail.com>
2020-08-18 14:56:56 +0100

    Updates

---
+++ precache-manifest.e9c10e700749b22c7d8031aa7817e5c1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da2ea64dffe6b4c1b7a85d74310fe0d",
+    "url": "/react-basics-course/index.html"
+  },
+  {
+    "revision": "69ea706026063a184163",
+    "url": "/react-basics-course/static/css/main.ae211235.chunk.css"
+  },
+  {
+    "revision": "6340aff5a81a65ab768f",
+    "url": "/react-basics-course/static/js/2.bac39102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basics-course/static/js/2.bac39102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ea706026063a184163",
+    "url": "/react-basics-course/static/js/main.dc74217e.chunk.js"
+  },
+  {
+    "revision": "be4dfc41f7cfd8d999cb",
+    "url": "/react-basics-course/static/js/runtime-main.9ff59eb8.js"
+  },
+  {
+    "revision": "c9d67fed737371f669f23983a8087b93",
+    "url": "/react-basics-course/static/media/1.c9d67fed.jpg"
+  },
+  {
+    "revision": "3d074a01cddf7182aa9396e6ed20723b",
+    "url": "/react-basics-course/static/media/10.3d074a01.jpg"
+  },
+  {
+    "revision": "a5eeb75058c0c3f462e504820540553b",
+    "url": "/react-basics-course/static/media/2.a5eeb750.jpg"
+  },
+  {
+    "revision": "805a7e97cda274068474bb845c396ce4",
+    "url": "/react-basics-course/static/media/3.805a7e97.jpg"
+  },
+  {
+    "revision": "63b241112b458f7580103c79c4678341",
+    "url": "/react-basics-course/static/media/4.63b24111.jpg"
+  },
+  {
+    "revision": "1f0d018a500c795bdb645d28d7ff2f59",
+    "url": "/react-basics-course/static/media/5.1f0d018a.jpg"
+  },
+  {
+    "revision": "db81d972380f0858d3e9df1278905797",
+    "url": "/react-basics-course/static/media/6.db81d972.jpg"
+  },
+  {
+    "revision": "d6b83820a6abb9752543b0d5c22a02a0",
+    "url": "/react-basics-course/static/media/7.d6b83820.jpg"
+  },
+  {
+    "revision": "4d09ccd29e37df76e973b25e163dc925",
+    "url": "/react-basics-course/static/media/8.4d09ccd2.jpg"
+  },
+  {
+    "revision": "eee6369342010e3603fbb4e0add59c8a",
+    "url": "/react-basics-course/static/media/9.eee63693.jpg"
+  }
+]);
